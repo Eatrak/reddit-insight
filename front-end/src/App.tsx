230e999a829bcb3741fff9f0cc05cca99382487a
unknown <davidevacirca8@gmail.com>
2026-01-22 13:11:27 +0100

    feat: add dark-mode in front-end

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,10 @@
-import './App.css'
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   return (
-    <div></div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div></div>
+    </ThemeProvider>
   )
 }
 
